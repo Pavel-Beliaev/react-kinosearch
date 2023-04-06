6eaad7b6a2c861b49cca6aa08c22396b04cac9bf
Павел Беляев <philipsxenium93@gmail.com>
2023-04-06 13:05:43 +0700

    add api kinosearch

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
+
 const Home = () => {
+
+        fetch('https://api.kinopoisk.dev/v1/movie/random', {
+                method: 'GET',
+                headers: {
+                        'Accept': 'application/json',
+                        'X-API-KEY': 'ZZ11XDW-57R4FQJ-MAQGVAK-12EKRXX'
+                }
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+
     return (
         <div>
             <br/>
